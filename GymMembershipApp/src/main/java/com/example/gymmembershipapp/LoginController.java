cc977210dbfbe838053d4249016b3587b5dd6827
Miroslav <kolosnjaji@stcable.net>
2024-01-26 23:06:50 +0100

    renamed Main app class to > GymMembershipApplication and renamed controller class to > LoginController

---
+++ GymMembershipApp/src/main/java/com/example/gymmembershipapp/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.gymmembershipapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void close() {
+        System.exit(0);
+    }
+}
